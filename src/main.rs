f565a34ad38bafb56316b8bb35ade248339dee66
Mitya <dmitrij.mal.tim.04@gmail.com>
2023-08-16 17:54:04 +0300

    123

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
-
+use std::io::Write;
 use eframe::egui;
+use std::fs::{File, OpenOptions};
+use std::io::Read;
 fn main() -> Result<(), eframe::Error> {
+    //let mut file = File::create("data/data.txt").expect("create failed");
+    //file.write_all("Hello World".as_bytes()).expect("write failed");
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
     let options = eframe::NativeOptions {
         initial_window_size: Some(egui::vec2(800.0, 600.0)),
@@ -15,14 +19,12 @@ fn main() -> Result<(), eframe::Error> {
 }
 
 struct MyApp {
-    name: String,
     age: u32,
 }
 
 impl Default for MyApp {
     fn default() -> Self {
         Self {
-            name: "Arthur".to_owned(),
             age: 42,
         }
     }
@@ -44,19 +46,26 @@ impl eframe::App for MyApp {
                         });
                         ui.group(|ui| {
                             ui.label("Общая информация");
+                            ui.label(format!("{}",self.age));
                             ui.set_min_height(200.0);
                         });
-                        let mut col = egui::collapsing_header::CollapsingHeader::new("Сотрудники")
+                        let _col = egui::collapsing_header::CollapsingHeader::new("Сотрудники")
                         .id_source(id)
                         .show(ui, |ui| {
                             ui.label("их пока нет");
-
-                            if ui.button("Click me").clicked() {
-                                ui.label("их пока нет, но ты создал");
+                            //let mut but = ui.button("go");
+                            if ui.button(format!("{}",id)).clicked() {
+                                self.age += id as u32;
+                                let mut file = OpenOptions::new().read(true).write(true).create(true).open("data/data.txt").unwrap();
+                                //File::open("data/data.txt").expect("create failed");
+                                let mut str1 = String::new();
+                                file.read_to_string(&mut str1).expect("write failed");
+                                file.write_all(format!("{}\n",self.age).as_bytes()).expect("write failed");
+                                //writeln!(file,"{}",format!("{}",self.age));
                             };
                         });
                     });
-                }
+                };
                 // let name_label = ui.label("Your name: ");
                 // ui.text_edit_singleline(&mut self.name)
                 //     .labelled_by(name_label.id);
